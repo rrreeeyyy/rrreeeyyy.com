223094bf8290db81ca0b41456975fd3582ffcf9e
Ryota Yoshikawa <yoshikawa@topotal.com>
2023-02-13 13:41:42 +0900

    Add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import yaml from '@rollup/plugin-yaml';
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 import react from "@astrojs/react";
@@ -10,7 +11,17 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rrreeeyyy.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),],
   vite: {
     plugins: [yaml()]
   }
